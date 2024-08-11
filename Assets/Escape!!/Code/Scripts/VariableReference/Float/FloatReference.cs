64e9577824a4a6bbb33df3174d92202aaebddb8c
MitchdeKock <159277915+MitchdeKock@users.noreply.github.com>
2024-08-11 12:46:38 +0200

    Event and float reference

---
+++ Assets/Escape!!/Code/Scripts/VariableReference/Float/FloatReference.cs
@@ -0,0 +1,23 @@
+[System.Serializable]
+public class FloatReference
+{
+    public bool UseConstant = true;
+    public float ConstantValue;
+    public FloatVariable Variable;
+
+    public float Value
+    {
+        get { return UseConstant ? ConstantValue : Variable.Value; }
+        set
+        {
+            if (UseConstant)
+            {
+                ConstantValue = value;
+            }
+            else
+            {
+                Variable.Value = value;
+            }
+        }
+    }
+}
