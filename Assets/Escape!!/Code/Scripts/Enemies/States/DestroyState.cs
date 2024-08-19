59ef1bb429b5f414ff36f95f1b39cd63d82a6fac
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-08-19 20:55:01 +0200

    particles from enemy to player
    On death particles flow from emey to player to show the transfer of energy

---
+++ Assets/Escape!!/Code/Scripts/Enemies/States/DestroyState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class DestroyState : IState
+{
+    private readonly MonoBehaviour _monoBehaviour;
+
+    public DestroyState(MonoBehaviour monoBehaviour)
+    {
+        _monoBehaviour = monoBehaviour;
+    }
+
+    public void Tick() { }
+
+    public void OnEnter()
+    {
+        GameObject.Destroy(_monoBehaviour.gameObject);
+    }
+
+    public void OnExit() { }
+}
