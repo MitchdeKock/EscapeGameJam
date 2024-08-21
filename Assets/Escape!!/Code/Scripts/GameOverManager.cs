ba7e54d2fefc1bdd8870e4950f11c4f22fd60f00
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-08-21 21:55:08 +0200

    Add basic gameover logic

---
+++ Assets/Escape!!/Code/Scripts/GameOverManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    [SerializeField] GameObject gameOverScreen;
+    // [SerializeField] TextMeshPro currentScore;
+    [SerializeField] TextMeshPro finalScore;
+
+    private CoreHealthHandler _coreHealth;
+    private void Start()
+    {
+        _coreHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<CoreHealthHandler>();
+        _coreHealth.OnHealthValueChanged += isGameOver;
+    }
+    public void isGameOver(int health)
+    {
+        if (health<=0)
+        {
+            SetGameOver();
+        }
+    }
+    public void SetGameOver()
+    {
+        PauseManager.Pause();
+        gameOverScreen.SetActive(true);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetSceneByName("Game").buildIndex);
+    }
+}
