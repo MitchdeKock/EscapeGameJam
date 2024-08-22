fe35187caf6115f015d52eeb862ffa32311db16c
Lior-Becker <lior.becker@bbd.co.za>
2024-08-22 22:04:36 +0200

    played flashes red when damage is taken

--- Assets/Escape!!/Code/Scripts/Core 1/CoreHealthHandler.cs
+++ Assets/Escape!!/Code/Scripts/Core 1/CoreHealthHandler.cs
@@ -8,13 +8,37 @@ public class CoreHealthHandler : MonoBehaviour
 {
     [SerializeField] private int health = 1;
     [SerializeField] private int maxHealth = 20;
+    private SpriteRenderer Headsprite;
+    private SpriteRenderer BodySprite;
+    public IEnumerator Flash()
+    {
+        Color oldColourHead = Headsprite.color;
+        Color oldColourBody = BodySprite.color;
 
+        Headsprite.color = Color.red;
+        BodySprite.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        Headsprite.color = oldColourHead;
+        BodySprite.color = oldColourBody;
+    }
+    private void Start()
+    {
+        Transform spriteHeadTransform = transform.Find("Sprite_head");
+        Transform spriteBodyTransform = transform.Find("Sprite_body");
+
+        Headsprite = spriteHeadTransform.GetComponent<SpriteRenderer>();
+        BodySprite = spriteBodyTransform.GetComponent<SpriteRenderer>();
+    }
     public int Health 
     { 
         get => health;
         set
         {
+            int oldHealth = health;
             health = Math.Min(value, maxHealth);
+            if (oldHealth - health>0 && oldHealth-health<10) {
+                StartCoroutine(Flash());
+            }
             OnHealthValueChanged?.Invoke(health);
         }
     }
