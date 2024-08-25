6ebeead80a5da19dc0b4e640dfa123a35ca07dad
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-08-25 10:21:32 +0200

    Added menu music
    Added sfx to play when adjusting the slider

--- Assets/Escape!!/Code/Scripts/SoundMixerManager.cs
+++ Assets/Escape!!/Code/Scripts/SoundMixerManager.cs
@@ -6,14 +6,15 @@ using UnityEngine.Audio;
 public class SoundMixerManager : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
-
+    [SerializeField] private AudioClip testSFX;
     public void SetMasterVolume(float level)
     {
         audioMixer.SetFloat("MasterVolume",level);
     }
     public void SetSFXVolume(float level)
-    {
+    { 
         audioMixer.SetFloat("SoundFXVolume", level);
+        SFXManager.instance.PlaySoundFXClip(testSFX, transform, 1f);
     }
     public void SetMusicVolume(float level)
     {
